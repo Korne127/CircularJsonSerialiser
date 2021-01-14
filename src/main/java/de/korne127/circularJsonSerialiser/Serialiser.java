8e827d4793c5f0eb249c62e1739188fcfa9de394
Kornelius Rohrschneider <Korne127@gmx.net>
2021-01-14 01:35:55 +0100

    Initial Commit
    Das Skelett für die Serialiser-Klasse wurde hinzugefügt.

---
+++ src/main/java/de/korne127/circularJsonSerialiser/Serialiser.java
@@ -0,0 +1,15 @@
+package de.korne127.circularJsonSerialiser;
+
+public class Serialiser {
+
+	public String serialiseObject(Object object) {
+
+		return null;
+	}
+
+	public Object deserialiseObject(String content) {
+
+		return null;
+	}
+
+}
